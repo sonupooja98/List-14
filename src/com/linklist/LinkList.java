a2bd0315edffc331c9d0fffbc684c7027047a088
sonupooja <sonupooja2398@gmail.com>
2021-10-15 22:58:13 +0530

    Add at end

--- src/com/linklist/LinkList.java
+++ src/com/linklist/LinkList.java
@@ -1,6 +1,10 @@
 package com.linklist;
 
+import java.util.Scanner;
 
+/*
+ * Created Node class
+ */
 class Node{ 
 	int data;
 	Node next;
@@ -9,6 +13,7 @@ class Node{
 		this.next=null;
 	}
 }
+
 class operations{
 
 
@@ -16,21 +21,21 @@ class operations{
 	 * Taking variables to store the head 
 	 */
 	Node head;
+	Node tail;
 
 	public void pushData(int data) {
 
-		
+		//created the object of the Node class to get the address of the Node and passing the "data" it will store in constructor
 		Node newnode= new Node(data);
 
 		if(head == null) {
 
 			head=newnode;
-
 		}
 		else {
-					/*
-					 * swapping the value to get the new node address 
-					 */
+			/*
+			 * swapping the value to get the new node address 
+			 */
 			Node temp= head;
 			this.head=newnode;
 			newnode.next=temp;
@@ -54,23 +59,77 @@ class operations{
 
 		}
 
+	}
 
+	public void append(int data) {
+
+		Node newnode=new Node(data);
+		if (head == null) {
+			head=newnode;
+			tail=newnode;
+		}
+		else {
+			tail.next=	newnode;
+			tail=newnode;
+
+		}
 	}
 
 }
 
 
 public class LinkList {
+	
+public static void addDataStart() {	
+	operations link=new operations ();
+	link.pushData(70);
+	link.pushData(30);
+	link.pushData(56);
+	link.print();
+}
+	
+public static void addDataEnd() {	
+	operations link=new operations ();
+	link.append(56);
+	link.append(30);
+	link.append(70);
+	link.print();
+}
+	
 
 	public static void main(String[] args) {
 
 		System.out.println("*** Welcome To LinkList Program ***");
+		Scanner sc= new Scanner(System.in);
+		System.out.println();
+		System.out.println("Press 1 to add data at start");
+		System.out.println("Press 2 to add data at end");
+		int input=sc.nextInt();
+		
+		switch (input) {
+		
+		/*
+		 * if user press 1 then addDataStart method will execute data will be store at starting
+		 */
+		
+		case 1:
+			 addDataStart() ;
+			break;
 
-		operations link=new operations ();
-		link.pushData(70);
-		link.pushData(30);
-		link.pushData(56);
-		link.print();
+			/*
+			 * if user press 2 then addDataEnd method will execute data will be store 
+			 */
+			
+		case 2:
+		
+			addDataEnd();
+			break;
+
+		default:
+			System.out.println("Invalid Choice");
+			
+		}
+		
 	}
 
 }
