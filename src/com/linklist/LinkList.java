96fe192e26c86837324ffe5082a097e37f3b2b39
sonupooja <sonupooja2398@gmail.com>
2021-10-15 22:48:27 +0530

    Linked List

---
+++ src/com/linklist/LinkList.java
@@ -0,0 +1,31 @@
+package com.linklist;
+
+public class LinkList {
+
+	public static void main(String[] args) {
+
+		System.out.println("*** Welcome To LinkList Program ***");
+
+		Node firstnode= new Node(56);
+		Node secondnode= new Node(30);
+		Node thirdnode= new Node(70);
+
+
+		/*
+		 * created a head variable to store the first node address
+		 * then we are storing the address of next nodes in variable next
+		 * 
+		 */
+		Node head=firstnode; 
+		firstnode.next=secondnode;
+		secondnode.next=thirdnode;
+
+		Node temp= head;// storing the head address into temp variable for print the values
+		while(temp!=null)
+		{
+			System.out.print(" -> "+temp.data);
+			temp=temp.next;
+		}
+
+	}
+}
