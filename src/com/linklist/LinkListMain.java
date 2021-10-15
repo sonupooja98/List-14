091b3eea23edc7d85b0ae9e3137cde5ad9d4dcdc
sonupooja <sonupooja2398@gmail.com>
2021-10-15 23:05:45 +0530

    Add b/w node

--- src/com/linklist/LinkListMain.java
+++ src/com/linklist/LinkListMain.java
@@ -12,7 +12,17 @@ class Node{
 		this.data=data;
 		this.next=null;
 	}
+
+	@Override
+	public String toString() {
+		return "Node{" +
+				"data=" + data +
+				", next=" + next;
+	}
+
+
 }
+
 
 class operations{
 
@@ -23,14 +33,14 @@ class operations{
 	Node head;
 	Node tail;
 
-	public void pushData(int data) {
+	public Node pushData(int data) {
 
 		//created the object of the Node class to get the address of the Node and passing the "data" it will store in constructor
 		Node newnode= new Node(data);
-
-		if(head == null) {
-
+		if(head == null) 
+		{
 			head=newnode;
+			tail=newnode;
 		}
 		else {
 			/*
@@ -41,6 +51,7 @@ class operations{
 			newnode.next=temp;
 
 		}
+		return newnode;
 
 	}
 
@@ -75,61 +86,103 @@ class operations{
 		}
 	}
 
+
+	public void inBetweenInsert(Node previousNode ,Node newNode) {
+		Node tempNode = previousNode.next;
+		previousNode.next = newNode;
+		newNode.next = tempNode;
+
+	}
+
 }
 
 
-public class LinkList {
-	
-public static void addDataStart() {	
-	operations link=new operations ();
-	link.pushData(70);
-	link.pushData(30);
-	link.pushData(56);
-	link.print();
-}
-	
-public static void addDataEnd() {	
-	operations link=new operations ();
-	link.append(56);
-	link.append(30);
-	link.append(70);
-	link.print();
-}
-	
 
+
+class LinkList {
+	static operations link=new operations ();
+
+	public static void addDataStart() {	
+		link.pushData(70);
+		link.pushData(30);
+		link.pushData(56);
+		link.print();
+	}
+
+	public void addDataEnd() {	
+
+		link.append(56);
+		link.append(30);
+		link.append(70);
+		link.print();
+	}
+
+	public void insertInBetewwn() {
+
+		Node secondnode=link.pushData(70);
+
+		Node firstnode=link.pushData(56);
+		Node newnode= new Node(30);
+
+
+		System.out.println("Before:");
+		link.print();  
+		link.inBetweenInsert(firstnode, newnode);
+		System.out.println("\nAfter:");
+		link.print();  
+
+	}
+
+
+
+}
+
+
+public	class LinkListMain{
 	public static void main(String[] args) {
+
+		LinkList obj= new LinkList();
+
 
 		System.out.println("*** Welcome To LinkList Program ***");
 		Scanner sc= new Scanner(System.in);
 		System.out.println();
 		System.out.println("Press 1 to add data at start");
 		System.out.println("Press 2 to add data at end");
+		System.out.println("Press 3 to insert data in between");
+		
 		int input=sc.nextInt();
-		
+
 		switch (input) {
-		
+
 		/*
 		 * if user press 1 then addDataStart method will execute data will be store at starting
 		 */
-		
+
 		case 1:
-			 addDataStart() ;
+			obj.addDataStart() ;
 			break;
 
 			/*
 			 * if user press 2 then addDataEnd method will execute data will be store 
 			 */
-			
+
 		case 2:
-		
-			addDataEnd();
+
+			obj.addDataEnd();
 			break;
+
+
+		case 3:
+
+			obj.insertInBetewwn();
+			break;		
 
 		default:
 			System.out.println("Invalid Choice");
-			
+
 		}
-		
+
 	}
 
 }
